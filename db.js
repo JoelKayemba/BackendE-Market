a3e1a14bc9fac509d5356bdbec29f8cab132105b
kayembajoel92@gmail.com <kayembajoel92@gmail.com>
2024-07-24 21:42:43 -0400

    backend

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Jojokay92',
+  password: 'TonMotDePasse',
   database: 'E-Market'
 });
 
