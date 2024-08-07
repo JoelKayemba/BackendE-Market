659dab65d3257c3486a919cf8c1523b619dbf42c
kayembajoel92@gmail.com <kayembajoel92@gmail.com>
2024-08-07 15:10:55 -0400

    adresse

--- routes/auth.js
+++ routes/auth.js
@@ -85,10 +85,210 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ message: 'Email ou mot de passe incorrect' });
     }
 
-    const token = jwt.sign({ id: user.id }, 'your_jwt_secret', { expiresIn: '1h' });
+    const token = jwt.sign({ id: user.idclient }, 'your_jwt_secret', { expiresIn: '1h' });
 
-    res.status(200).json({ message: 'Connexion réussie', token });
+    res.status(200).json({ message: 'Connexion réussie', token, userId: user.idclient });
   });
 });
+
+
+// Route pour ajouter une adresse
+router.post('/adresses', async (req, res) => {
+  const {
+    userId,
+    pays,
+    province,
+    ville,
+    rue,
+    numero,
+    codePostal,
+    appartement,
+    latitude,
+    longitude,
+    parDefaut
+  } = req.body;
+
+  // Validation des champs requis
+ /* if (!userId || !pays || !province || !ville || !rue || !numero || !latitude || !longitude || typeof parDefaut === 'undefined') {
+    return res.status(400).json({ message: 'Tous les champs obligatoires doivent être remplis.' });
+  }*/
+
+  try {
+    const insertAddressQuery = `
+      INSERT INTO adresse (
+        pays, province, ville, rue, numéro, code_postal, appartement, latitude, longitude, parDefaut, idclient
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+    const values = [
+      pays, province, ville, rue, numero, codePostal, appartement, latitude, longitude, parDefaut, userId
+    ];
+
+    connection.query(insertAddressQuery, values, async (err, results) => {
+      if (err) {
+        console.error('Erreur lors de l\'insertion de l\'adresse:', err);
+        return res.status(500).json({ message: 'Erreur lors de l\'ajout de l\'adresse' });
+      }
+
+      // Vérifiez si results.insertId contient l'ID inséré
+      if (!results.insertId) {
+        return res.status(500).json({ message: 'ID manquant pour l\'adresse ajoutée' });
+      }
+
+      // Récupérer les détails de l'adresse nouvellement insérée
+      const selectAddressQuery = `
+        SELECT * FROM adresse WHERE idadresse = ?
+      `;
+      connection.query(selectAddressQuery, [results.insertId], (err, rows) => {
+        if (err) {
+          console.error('Erreur lors de la récupération des détails de l\'adresse:', err);
+          return res.status(500).json({ message: 'Erreur lors de la récupération des détails de l\'adresse' });
+        }
+
+        // Renvoyer les détails de l'adresse
+        res.status(201).json(rows[0]);
+      });
+    });
+  } catch (error) {
+    console.error('Erreur inattendue:', error);
+    res.status(500).json({ message: 'Erreur inattendue' });
+  }
+});
+
+
+
+//route pour mettre a jour l'adresse par defaut
+router.patch('/adresses/:id/parDefaut', async (req, res) => {
+  const { id } = req.params;
+  const { parDefaut } = req.body;
+
+  if (parDefaut === undefined) {
+    return res.status(400).json({ message: 'Le champ parDefaut est requis.' });
+  }
+
+  try {
+    // Vérifiez si l'adresse existe
+    const checkAddressQuery = 'SELECT * FROM adresse WHERE idadresse = ?';
+    connection.query(checkAddressQuery, [id], (err, results) => {
+      if (err) {
+        console.error('Erreur lors de la vérification de l\'adresse:', err);
+        return res.status(500).json({ message: 'Erreur lors de la vérification de l\'adresse' });
+      }
+
+      if (results.length === 0) {
+        return res.status(404).json({ message: 'Adresse non trouvée.' });
+      }
+
+      // Récupérez l'ID du client de l'adresse
+      const userId = results[0].idclient;
+
+      // Mettre à jour toutes les adresses de l'utilisateur pour ne pas être par défaut
+      const updateAllAddressesQuery = 'UPDATE adresse SET parDefaut = ? WHERE idclient = ?';
+      connection.query(updateAllAddressesQuery, [false, userId], (err) => {
+        if (err) {
+          console.error('Erreur lors de la mise à jour des adresses de l\'utilisateur:', err);
+          return res.status(500).json({ message: 'Erreur lors de la mise à jour des adresses' });
+        }
+
+        // Mettre à jour l'adresse spécifique pour qu'elle soit par défaut
+        const updateAddressQuery = 'UPDATE adresse SET parDefaut = ? WHERE idadresse = ?';
+        connection.query(updateAddressQuery, [parDefaut, id], (err) => {
+          if (err) {
+            console.error('Erreur lors de la mise à jour de l\'adresse:', err);
+            return res.status(500).json({ message: 'Erreur lors de la mise à jour de l\'adresse' });
+          }
+
+          // Récupérer les détails de l'adresse mise à jour
+          const selectAddressQuery = 'SELECT * FROM adresse WHERE idadresse = ?';
+          connection.query(selectAddressQuery, [id], (err, rows) => {
+            if (err) {
+              console.error('Erreur lors de la récupération des détails de l\'adresse:', err);
+              return res.status(500).json({ message: 'Erreur lors de la récupération des détails de l\'adresse' });
+            }
+
+            // Renvoyer les détails de l'adresse mise à jour
+            res.status(200).json(rows[0]);
+          });
+        });
+      });
+    });
+  } catch (error) {
+    console.error('Erreur inattendue:', error);
+    res.status(500).json({ message: 'Erreur inattendue' });
+  }
+});
+
+
+// Route pour mettre à jour une adresse
+router.put('/adresses/:id', async (req, res) => {
+  const { id } = req.params;
+  const {
+    pays,
+    province,
+    ville,
+    rue,
+    numero,
+    codePostal,
+    appartement,
+    latitude,
+    longitude,
+    parDefaut
+  } = req.body;
+
+  // Validation des champs requis
+  /*if (!pays || !province || !ville || !rue || !numero || !latitude || !longitude) {
+    return res.status(400).json({ message: 'Tous les champs obligatoires doivent être remplis.' });
+  }*/
+
+  try {
+    // Vérifiez si l'adresse existe
+    const checkAddressQuery = 'SELECT * FROM adresse WHERE idadresse = ?';
+    connection.query(checkAddressQuery, [id], (err, results) => {
+      if (err) {
+        console.error('Erreur lors de la vérification de l\'adresse:', err);
+        return res.status(500).json({ message: 'Erreur lors de la vérification de l\'adresse' });
+      }
+
+      if (results.length === 0) {
+        return res.status(404).json({ message: 'Adresse non trouvée.' });
+      }
+
+      // Mettre à jour l'adresse
+      const updateAddressQuery = `
+        UPDATE adresse SET
+          pays = ?, province = ?, ville = ?, rue = ?, numero = ?, code_postal = ?, appartement = ?,
+          latitude = ?, longitude = ?, parDefaut = ?
+        WHERE idadresse = ?
+      `;
+      const values = [
+        pays, province, ville, rue, numero, codePostal, appartement, latitude, longitude, parDefaut, id
+      ];
+
+      connection.query(updateAddressQuery, values, (err) => {
+        if (err) {
+          console.error('Erreur lors de la mise à jour de l\'adresse:', err);
+          return res.status(500).json({ message: 'Erreur lors de la mise à jour de l\'adresse' });
+        }
+
+        // Récupérer les détails de l'adresse mise à jour
+        const selectAddressQuery = 'SELECT * FROM adresse WHERE idadresse = ?';
+        connection.query(selectAddressQuery, [id], (err, rows) => {
+          if (err) {
+            console.error('Erreur lors de la récupération des détails de l\'adresse:', err);
+            return res.status(500).json({ message: 'Erreur lors de la récupération des détails de l\'adresse' });
+          }
+
+          // Renvoyer les détails de l'adresse mise à jour
+          res.status(200).json(rows[0]);
+        });
+      });
+    });
+  } catch (error) {
+    console.error('Erreur inattendue:', error);
+    res.status(500).json({ message: 'Erreur inattendue' });
+  }
+});
+
+
+
 
 module.exports = router;
